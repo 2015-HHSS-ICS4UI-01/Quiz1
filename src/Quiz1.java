53a39bba54f65b5447383d17c46b4a808b2b0430
donet6376 <donet6376@HRH-IC0027324.wrdsb.ca>
2015-09-22 11:18:43 -0400

    Completed

--- src/Quiz1.java
+++ src/Quiz1.java
@@ -21,31 +21,31 @@ public class Quiz1 {
         while(true)
         {
           System.out.println("Enter in a number to classify");
-          int num = input.nextInt();
+          int num = input.nextInt(); // stores the inputted number as an integer
               
-          if(num == 0)
-          {
+          if(num == 0) 
+          {   //if the integer is 0, end the program
               return;
           }else
           {
-              int twice = num * 2;
-              int sum = 0;
+              int twice = num * 2; //creates an integer that is twice the number
+              int sum = 0; //creates the currently empty integer for the sum
               for(int i = 1; i <= num; i++)
-              {
-                   int temp = num % i;
-                   if(temp == 0)
+              {    //checking if the number "i" is a divisor to the number
+                   int div = num % i;
+                   if(div == 0)
                    {
-                       sum = sum + i;
+                       sum = sum + i; //add the divisor "i" to the sum total
                    }
               }
               if(sum > twice)
-              {
-                  System.out.println(num + " is an abundant number");
+              {   //outputs when the sum of digits is greater than twice the number
+                  System.out.println(num + " is an abundant number"); 
               }else if(sum == twice)
-              {
+              {   //outputs when the sum of digits is equal to twice the number
                   System.out.println(num + " is a perfect number");      
               }else if(sum < twice)
-              {
+              {   //outputs when the sum of digits is less than twice the number
                   System.out.println(num + " is a deficient number");
               }
           }
